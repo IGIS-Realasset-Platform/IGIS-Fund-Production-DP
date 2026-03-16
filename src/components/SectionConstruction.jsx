eabb16e0d8ebc82a14a1f2234435f7b741e9ba94
this8369 <this8369@hotmail.com>
2026-03-16 16:45:00 +0900

    feat(ui): add image source text 'Tadawul Tower' to construction section

--- src/components/SectionConstruction.jsx
+++ src/components/SectionConstruction.jsx
@@ -15,6 +15,11 @@ export default function SectionConstruction() {
                     style={{ backgroundImage: "url('./img/samsung_cnt.jpg')" }}
                 ></div>
 
+                {/* Image Source */}
+                <div className="absolute bottom-6 right-8 md:right-[5%] xl:right-[10%] z-20 text-[10px] text-gray-500/80 font-light tracking-tight pointer-events-none">
+                    {lang === 'kr' ? "사우디 Tadawul Tower(삼성물산 시공)" : "Saudi Tadawul Tower (Constructed by Samsung C&T)"}
+                </div>
+
                 {/* Text Overlay Box (Right side) */}
                 <div className="absolute right-0 top-0 bottom-0 w-[55%] flex flex-col justify-center pr-[5%] xl:pr-[10%] pl-8 z-10">
                     <div className="w-full max-w-[800px] bs-fade-up delay-100">
@@ -57,7 +62,7 @@ export default function SectionConstruction() {
 
                         {/* overlay image layer */}
                         <div className="w-full mt-2 bs-fade-up delay-300">
-                            <img  src="./img/hyundai&samsung.png" width="1201" height="614" alt="Hyundai & Samsung Logos and Ranking" className="w-[100%] max-w-[700px] h-auto object-contain block" />
+                            <img src="./img/hyundai&samsung.png" width="1201" height="614" alt="Hyundai & Samsung Logos and Ranking" className="w-[100%] max-w-[700px] h-auto object-contain block" />
                         </div>
                     </div>
                 </div>
@@ -68,7 +73,12 @@ export default function SectionConstruction() {
             <div className="block lg:hidden w-full flex flex-col pb-[100px] bg-white">
                 {/* Image Section */}
                 <div className="w-full h-[55vh] min-h-[400px] relative shrink-0">
-                    <img  src="./img/samsung_cnt.jpg" width="3000" height="1984" alt="Construction Background" className="absolute inset-0 w-full h-full object-cover object-bottom" />
+                    <img src="./img/samsung_cnt.jpg" width="3000" height="1984" alt="Construction Background" className="absolute inset-0 w-full h-full object-cover object-bottom" />
+
+                    {/* Image Source */}
+                    <div className="absolute bottom-3 right-4 z-20 text-[10px] text-gray-500/80 font-light tracking-tight pointer-events-none">
+                        {lang === 'kr' ? "사우디 Tadawul Tower(삼성물산 시공)" : "Saudi Tadawul Tower (Constructed by Samsung C&T)"}
+                    </div>
                 </div>
 
                 {/* Text Section */}
@@ -113,7 +123,7 @@ export default function SectionConstruction() {
 
                         {/* overlay image layer */}
                         <div className="w-full mx-auto mt-4 bs-fade-up">
-                            <img  src="./img/hyundai&samsung.png" width="1201" height="614" alt="Hyundai & Samsung Logos and Ranking" className="w-full h-auto object-contain block mx-auto" />
+                            <img src="./img/hyundai&samsung.png" width="1201" height="614" alt="Hyundai & Samsung Logos and Ranking" className="w-full h-auto object-contain block mx-auto" />
                         </div>
                     </div>
                 </div>
@@ -125,7 +135,7 @@ export default function SectionConstruction() {
 
                     {/* Image 1 - Left (Using its natural aspect ratio so it doesn't crop its width) */}
                     <div className="w-full relative group overflow-hidden md:aspect-[2200/2630]">
-                        <img  src="./img/construct_reference01.jpg" width="2200" height="2630" alt="Construction Reference 1" className="w-full h-auto md:absolute md:inset-0 md:h-full object-cover object-top block transform transition-transform duration-[1.5s] group-hover:scale-105" />
+                        <img src="./img/construct_reference01.jpg" width="2200" height="2630" alt="Construction Reference 1" className="w-full h-auto md:absolute md:inset-0 md:h-full object-cover object-top block transform transition-transform duration-[1.5s] group-hover:scale-105" />
                     </div>
 
                     {/* Images 2, 3, 4 - Right Column */}
@@ -133,16 +143,16 @@ export default function SectionConstruction() {
 
                         {/* Image 2 - Right Top */}
                         <div className="w-full group overflow-hidden relative flex-1 min-h-[250px] md:min-h-0">
-                            <img  src="./img/construct_reference02.jpg" width="2070" height="1702" alt="Construction Reference 2" className="absolute inset-0 w-full h-full object-cover object-top block transform transition-transform duration-[1.5s] group-hover:scale-105" />
+                            <img src="./img/construct_reference02.jpg" width="2070" height="1702" alt="Construction Reference 2" className="absolute inset-0 w-full h-full object-cover object-top block transform transition-transform duration-[1.5s] group-hover:scale-105" />
                         </div>
 
                         {/* Images 3 & 4 - Right Bottom Split */}
                         <div className="w-full flex flex-row gap-[4px] flex-1 min-h-[250px] md:min-h-0">
                             <div className="w-1/2 group overflow-hidden relative">
-                                <img  src="./img/construct_reference03.jpg" width="1058" height="1292" alt="Construction Reference 3" className="absolute inset-0 w-full h-full object-cover object-top block transform transition-transform duration-[1.5s] group-hover:scale-105" />
+                                <img src="./img/construct_reference03.jpg" width="1058" height="1292" alt="Construction Reference 3" className="absolute inset-0 w-full h-full object-cover object-top block transform transition-transform duration-[1.5s] group-hover:scale-105" />
                             </div>
                             <div className="w-1/2 group overflow-hidden relative">
-                                <img  src="./img/construct_reference04.jpg" width="992" height="1292" alt="Construction Reference 4" className="absolute inset-0 w-full h-full object-cover object-top block transform transition-transform duration-[1.5s] group-hover:scale-105" />
+                                <img src="./img/construct_reference04.jpg" width="992" height="1292" alt="Construction Reference 4" className="absolute inset-0 w-full h-full object-cover object-top block transform transition-transform duration-[1.5s] group-hover:scale-105" />
                             </div>
                         </div>
                     </div>
