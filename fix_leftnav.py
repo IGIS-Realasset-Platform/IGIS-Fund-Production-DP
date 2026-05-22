4e4aa31186000f82e712b8cd6c241de8fc4b5fd4
이시정 Lee Si Jung <10137@IGISAM.COM>
2026-05-22 17:51:14 +0900

    Keep only mobile app and build artifact folders

--- fix_leftnav.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-path = 'src/components/system/IotaLeftNav.jsx'
-with open(path, 'r') as f:
-    content = f.read()
-
-# We need to find the correct 의사결정 로그 and change it
-# Let's see context around '의사결정 로그'
--- revert_override.mjs
+++
@@ -1,9 +0,0 @@
-import fs from 'fs';
-
-const file = 'src/components/system/VehicleIntegrated.jsx';
-let content = fs.readFileSync(file, 'utf8');
-
-const targetRegex = /\/\/\s*---\s*816 PFV Refinancing Mock Data Override\s*---[\s\S]*?\/\/\s*----------------------------------------------------/g;
-
-content = content.replace(targetRegex, '');
-fs.writeFileSync(file, content);
