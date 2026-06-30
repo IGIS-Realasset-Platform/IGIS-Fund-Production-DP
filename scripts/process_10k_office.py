780d5199bbc145c8119a1637fff674a11065c566
this8369 <this8369@hotmail.com>
2026-06-30 14:57:36 +0900

    ㅇ

---
+++ scripts/process_10k_office.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import re
+import time
+
+def process_office_data():
+    """
+    1만 평 이상 프라임 오피스 마스터 DB 정제 파이프라인 (Extreme Clean)
+    - 텍스트 주소 -> 위경도 변환 (카카오/구글 API 연동 예정)
+    - 건물명 오타 및 표기법 퍼지 매칭
+    - NOC 및 공실률 백그라운드 연산
+    """
+    print("[시스템] 오피스 1만평+ 데이터 정제 스크립트 백그라운드 대기 중...")
+    # 부장님의 지시 대기
+    pass
+
+if __name__ == "__main__":
+    process_office_data()
