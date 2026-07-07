7bca58810da55a5a5c0a5487f8ff9904d251bc20
this8369 <this8369@hotmail.com>
2026-07-07 09:45:33 +0900

    style: widen 세부업무 column width by 10px in timeline table

--- src/components/system/pmo/PmoScheduleGate.jsx
+++ src/components/system/pmo/PmoScheduleGate.jsx
@@ -449,14 +449,14 @@ export default function PmoScheduleGate() {
             {/* Timeline Matrix Grid */}
             <div className="-mr-[calc(50vw-50%)] border border-r-0 border-[#3c3c3c] bg-[#272726] rounded-l-[24px] overflow-hidden">
                 <div className="w-full overflow-x-auto pr-0 timeline-scrollbar">
-                    <div className="flex items-center min-w-[2260px]">
-                        <table className="text-left table-fixed min-w-[1460px] flex-1">
+                    <div className="flex items-center min-w-[2270px]">
+                        <table className="text-left table-fixed min-w-[1470px] flex-1">
                             <thead>
                                 <tr className="border-b border-[#3c3c3c] bg-transparent text-[#86868B] font-bold text-[13px] h-12">
                                     <th className="px-2 w-[100px] text-center sticky left-0 bg-[#272726] z-30">구분</th>
-                                    <th className="pl-4 w-[250px] sticky left-[100px] bg-[#272726] z-30">세부업무</th>
-                                    <th className="px-2 w-[110px] text-center sticky left-[350px] bg-[#272726] z-30">주관</th>
-                                    <th className="px-2 w-[100px] text-center sticky left-[460px] bg-[#272726] z-30 border-r border-[#3c3c3c] shadow-[4px_0_8px_-4px_rgba(0,0,0,0.5)]">협업</th>
+                                    <th className="pl-4 w-[260px] sticky left-[100px] bg-[#272726] z-30">세부업무</th>
+                                    <th className="px-2 w-[110px] text-center sticky left-[360px] bg-[#272726] z-30">주관</th>
+                                    <th className="px-2 w-[100px] text-center sticky left-[470px] bg-[#272726] z-30 border-r border-[#3c3c3c] shadow-[4px_0_8px_-4px_rgba(0,0,0,0.5)]">협업</th>
                                     {COLUMNS.map((col, cIdx) => {
                                         return (
                                             <th key={col.key} className={`text-center font-mono text-[11px] leading-tight px-1 font-bold w-[90px] ${
@@ -491,7 +491,7 @@ export default function PmoScheduleGate() {
                                             </td>
                                             
                                             {/* 주관 */}
-                                            <td className="px-2 text-[#E5E5E5] font-semibold text-center sticky left-[350px] bg-[#272726] group-hover:bg-[#333] transition-colors z-20 text-[13px] leading-tight whitespace-normal break-all">
+                                            <td className="px-2 text-[#E5E5E5] font-semibold text-center sticky left-[360px] bg-[#272726] group-hover:bg-[#333] transition-colors z-20 text-[13px] leading-tight whitespace-normal break-all">
                                                 {item.lead.includes(' ') ? (
                                                     item.lead.split(' ').map((part, pIdx) => (
                                                         <div key={pIdx}>{part}</div>
@@ -502,7 +502,7 @@ export default function PmoScheduleGate() {
                                             </td>
                                             
                                             {/* 협업 */}
-                                            <td className="px-2 text-[#86868B] leading-tight text-center pr-2 whitespace-normal break-all sticky left-[460px] bg-[#272726] group-hover:bg-[#333] transition-colors z-20 border-r border-[#3c3c3c] shadow-[4px_0_8px_-4px_rgba(0,0,0,0.5)]">
+                                            <td className="px-2 text-[#86868B] leading-tight text-center pr-2 whitespace-normal break-all sticky left-[470px] bg-[#272726] group-hover:bg-[#333] transition-colors z-20 border-r border-[#3c3c3c] shadow-[4px_0_8px_-4px_rgba(0,0,0,0.5)]">
                                                 {item.coop.split(';').map((c, cIdx) => (
                                                     c && <div key={cIdx} className="text-[11px]">{c}</div>
                                                 ))}
